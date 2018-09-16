87d94106baac21ae1c198f546d43600c71d85f31
Garrett Hughes <guitarguy13@ymail.com>
2018-09-16 17:19:38 -0500

    Scraper V2

--- scraper.js
+++ scraper.js
@@ -1,6 +1,7 @@
 let request = require('request');
 let cheerio = require('cheerio');
-let urls = [];
+let fs = require('fs');
+let dir = './data'
 let today = new Date();
 let dd = today.getDate();
 let mm = today.getMonth()+1;
@@ -10,38 +11,46 @@ let date = yyyy + '-' + mm + '-' + dd;
 const createCsvWriter = require('csv-writer').createArrayCsvWriter;
 const csvWriter = createCsvWriter({
     header: ['TITLE', 'PRICE', 'IMGURL', 'URL', 'TIME'],
-    path: 'data.csv'
+    path: 'data/' + date
 });
 let records = [];
 
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
 
-let createPromise = new Promise(function(resolve, reject) {
+let getshirtURLS = new Promise(function(resolve) {
         request('http://shirts4mike.com/shirts.php', function(error, response, body) {
+          if(error || response.statusCode != 200) {
+            console.error('Error: Could not connect to shirts4mike.com');
+            fs.appendFileSync('scraper-error.log', error.message + " " + new Date());
+            process.exit(1);
+          };
            let $ = cheerio.load(body);
            let shirts = $('.products').children();
-           resolve(shirts.each(function(index) {
+           shirts.each(function(index) {
              let url = (shirts.eq(index).children().eq(0).attr('href'));
-             request("http://shirts4mike.com/" + url, function(error, response, html) {
-               let $ = cheerio.load(html);
-               let shirtImg = $('.shirt-picture').children().eq(0).children().eq(0).attr('src');
-               let titleFull = $('.shirt-details').children().eq(0).text();
-               let titleArray = titleFull.split(' ');
-               let price = titleArray.splice(0, 1);
-               price = price.join(' ');
-               let title = titleArray.join(' ');
-               // console.log([title, price, shirtImg, url, date]);
-               records.push([title, price, shirtImg, url, date]);
-             });
-           }));
+             resolve(getData(url));
+           });//End of Resolve Statement
         });
-      })
+      });
 
+function getData(url) {
+  request("http://shirts4mike.com/" + url, function(error, response, html) {
+    let $ = cheerio.load(html);
+    let shirtImg = $('.shirt-picture').children().eq(0).children().eq(0).attr('src');
+    let titleFull = $('.shirt-details').children().eq(0).text();
+    let titleArray = titleFull.split(' ');
+    let price = titleArray.splice(0, 1).join(' ');
+    let title = titleArray.join(' ');
+    records.push([title, price, shirtImg, url, date]);
+  })
+};
 
-createPromise.then(function() {
-  console.log(records);
+getshirtURLS.then(setTimeout(function() {
+  // console.log(records)
   csvWriter.writeRecords(records)
-  console.log(records);
-});
+}, 1000));
 
 //create a csv file (csv creation module with at least 1k dl and updated in last 6 months)
 //npm install should install all our dependencies
